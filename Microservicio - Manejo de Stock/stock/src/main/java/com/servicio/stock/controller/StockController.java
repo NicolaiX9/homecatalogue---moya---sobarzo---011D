e68bd691b85bae003613866bd25449e9d7742ebd
Nxcxlxs17 <nico.sobarzo@duocuc.cl>
2026-05-10 23:40:19 -0400

    haciendo stock

--- Microservicio - Manejo de Stock/stock/src/main/java/com/servicio/stock/controller/StockController.java
+++ Microservicio - Manejo de Stock/stock/src/main/java/com/servicio/stock/controller/StockController.java
@@ -1,15 +1,10 @@
 package com.servicio.stock.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,10 +33,5 @@ public class StockController {
         return stockService.buscarPorId(idStock);
     }
 
-    @PutMapping("/{idStock}")
-    public ResponseEntity<Stock> actualizar(@PathVariable Long idStock, @RequestBody Stock stock){
-        try{
-            Stock sto = stockService.buscarPorId(idStock);
-        }
-    }
+    
 }
