91b21ae296bfc9376052b185a59a898b9a7ed97f
NicolaiX9 <nicolasmoyacartes@gmail.com>
2026-05-21 14:49:35 -0400

    Servicio distribuidores creado

---
+++ distribuidores/src/test/java/com/servicio/distribuidores/DistribuidoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.distribuidores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistribuidoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
