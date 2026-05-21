6288972af1e6f7990b074d67be59f99c12050fb4
NicolaiX9 <nicolasmoyacartes@gmail.com>
2026-05-21 14:46:38 -0400

    Suministros creado

---
+++ suministros/src/main/java/com/servicio/suministros/SuministrosApplication.java
@@ -0,0 +1,13 @@
+package com.servicio.suministros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuministrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuministrosApplication.class, args);
+	}
+
+}
