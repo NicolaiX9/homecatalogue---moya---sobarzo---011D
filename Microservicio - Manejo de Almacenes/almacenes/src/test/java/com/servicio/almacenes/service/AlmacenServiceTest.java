d201ecf022ffbf290a570316d032c03dc5d31e57
Nxcxlxs17 <nico.sobarzo@duocuc.cl>
2026-06-13 04:38:51 -0400

    Creacion de Test en Almacen

---
+++ Microservicio - Manejo de Almacenes/almacenes/src/test/java/com/servicio/almacenes/service/AlmacenServiceTest.java
@@ -0,0 +1,85 @@
+package com.servicio.almacenes.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.servicio.almacenes.model.Almacen;
+import com.servicio.almacenes.repository.AlmacenRepository;
+
+@ExtendWith(MockitoExtension.class) // usamos mockito para simular objetos
+public class AlmacenServiceTest {
+    @Mock
+    private AlmacenRepository almacenRepository;
+    @InjectMocks
+    private AlmacenService almacenService;
+    
+    
+    // ------- Creacion de metodos de prueba -------
+
+    // metodo de buscar por id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Test
+    @DisplayName("Deberia guardar el almacen correctamente")
+    // metodo del guardar = save
+    void guardarAlmacenTest(){
+        Almacen almacen =  new Almacen();
+        almacen.setCalleDireccion("Pajaritos");
+        almacen.setNumeroDireccion("7845");
+        when(almacenRepository.save(any(Almacen.class))).thenAnswer(invocation -> {
+            Almacen a = invocation.getArgument(0);
+            a.setId(1L);
+            return a;
+        });
+    
+    Almacen resultado = almacenService.crearAlmacen(almacen);
+    assertNotNull(resultado);
+    assertEquals(1L, resultado.getId());
+    assertEquals("Pajaritos", resultado.getCalleDireccion());
+    verify(almacenRepository, times(1)).save(almacen);
+    }
+
+    // metodo del eliminar = delete
+    @Test
+    @DisplayName("Deberia eliminar el almacen por su ID correctamente")
+    void eliminarAlmacenTest(){
+        Long almacenId = 1L;
+        doNothing().when(almacenRepository).deleteById(almacenId);
+        almacenService.eliminarAlmacen(almacenId);
+        verify(almacenRepository, times(1)).deleteById(almacenId);
+    }
+
+
+
+
+
+
+}
