d060338143e2f12a0782dacd9349ce62bcfa99e1
NicolaiX9 <nicolasmoyacartes@gmail.com>
2026-06-12 22:09:37 -0400

    Cambios Auth

--- service-auth/src/main/java/com/hospital/service_auth/dto/AuthRequest.java
+++ service-auth/src/main/java/com/hospital/service_auth/dto/AuthRequest.java
@@ -1,5 +1,19 @@
 package com.hospital.service_auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+//Aquí se guardan temporalmente los datos del Json que envía el usuario
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthRequest {
+
+    private String nombreUsuario;
+    private String password;
+
 
 }
