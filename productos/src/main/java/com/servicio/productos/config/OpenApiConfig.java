f0ea6b8c4f1ff8076e56c40ae0befbdd0db14fbc
NicolaiX9 <nicolasmoyacartes@gmail.com>
2026-06-21 07:13:47 -0400

    OpenApiConfig añadido

---
+++ productos/src/main/java/com/servicio/productos/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.servicio.productos.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI().info(new Info().title("API Home Catalogue - Servicio de Productos")
+        .version("1.0").description("Documentación centralizada del Software Home Catalogue"))
+        //Esto es lo que solicita el Failed to Fetch
+        .servers(List.of(new Server().url("http:localhost:9080").description("Servidor a través del Gateway")));
+    }
+
+
+
+}
