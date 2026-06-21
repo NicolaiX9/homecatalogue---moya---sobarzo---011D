363fdaee8e45f3d2515e6e3273c07b60a7b92b11
NicolaiX9 <nicolasmoyacartes@gmail.com>
2026-06-21 07:21:42 -0400

    Error Response Incorporated

---
+++ productos/src/main/java/com/servicio/productos/config/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.servicio.productos.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class ErrorResponse {
+
+    private int status;
+    private String message;
+    private long timestamp;
+
+
+}
